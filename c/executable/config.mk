72e1ef3e02997305a8736dfb5e178192641bc3f8
Paweł Cholewa <MCPlayG@wp.pl>
2019-01-05 20:36:31 +0100

    Added support for libraries and multiple include sources

--- c/executable/config.mk
+++ c/executable/config.mk
@@ -3,11 +3,15 @@
 SRC_DIR = src
 BIN_DIR = bin
 
+INCLUDE_DIRS = $(SRC_DIR)
+
 DEBUG ?= false
 DEBUG_MACRO_NAME = DEBUG
 
 CC = gcc
-CFLAGS = --std=c11 -I$(SRC_DIR)
+CSTATICLIBS =
+CLIBS =
+CFLAGS = --std=c11 $(addprefix -l,$(CLIBS)) $(addprefix -I,$(INCLUDE_DIRS))
 
 EXECUTABLE_NAME = test_exec
 INSTALL_PATH = /usr/local/bin/$(EXECUTABLE_NAME)
